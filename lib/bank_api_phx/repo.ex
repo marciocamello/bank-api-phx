57851c68e8f112f5f57b57edbbf636a1449f4f45
Marcio Camello <marciocamello@outlook.com>
2020-04-29 13:46:16 -0300

    Add seeds

--- lib/bank_api_phx/repo.ex
+++ lib/bank_api_phx/repo.ex
@@ -1,5 +1,14 @@
 defmodule BankApiPhx.Repo do
   use Ecto.Repo,
-    otp_app: :bank_api_phx,
-    adapter: Ecto.Adapters.Postgres
+      otp_app: :bank_api_phx,
+      adapter: Ecto.Adapters.Postgres
+
+  if Mix.env() in [:dev, :test] do
+    @spec truncate(Ecto.Schema.t()) :: :ok
+    def truncate(schema) do
+      table_name = schema.__schema__(:source)
+      query("TRUNCATE #{table_name} CASCADE", [])
+      :ok
+    end
+  end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,43 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias BankApiPhx.Repo
+alias BankApiPhx.Accounts
+alias BankApiPhx.Accounts.User
+
+Repo.truncate(BankApiPhx.Accounts.Account)
+Repo.truncate(BankApiPhx.Accounts.User)
+
+user1 = %{
+  email: "hermione@hogwarts.com",
+  firstName: "Hermione",
+  lastName: "Granger",
+  phone: "00 0000 0000",
+  password: "123123123"
+}
+
+{:ok, %User{} = account} =Accounts.create_user(user1)
+Accounts.bind_account(account)
+
+user2 = %{
+  email: "harrypotter@hogwarts.com",
+  firstName: "Harry",
+  lastName: "Potter",
+  phone: "00 0000 0000",
+  password: "123123123"
+}
+
+{:ok, %User{} = account} = Accounts.create_user(user2)
+Accounts.bind_account(account)
+
+admin = %{
+  email: "albusdumbledore@hogwarts.com",
+  firstName: "Albus",
+  lastName: "Dumbledore",
+  phone: "00 0000 0000",
+  password: "123123123",
+  acl: "admin"
+}
+
+Accounts.create_user(admin)
